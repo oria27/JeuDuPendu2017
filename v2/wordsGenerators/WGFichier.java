a75d5fc1afb04e51e19517fc3b460d5505e01ee3
Cyrille Giquello <cyrille@giquello.fr>
2017-02-15 16:45:13 +0100

    words generator #6

---
+++ v2/wordsGenerators/WGFichier.java
@@ -0,0 +1,13 @@
+package v2.wordsGenerators;
+
+import v2.WordsGenerator;
+
+public class WGFichier implements WordsGenerator {
+
+	@Override
+	public String getWord() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ v2/wordsGenerators/WGUserInput.java
@@ -0,0 +1,17 @@
+package v2.wordsGenerators;
+
+import v2.WordsGenerator;
+
+/**
+ * Saisie du mot au clavier 
+ *
+ */
+public class WGUserInput implements WordsGenerator {
+
+	@Override
+	public String getWord() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
